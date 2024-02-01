ee9ce927b7330e495eeaa263248e2ecc732f96ad
Filip Mrdak <fmrdak@gmail.com>
2024-02-01 19:29:06 +0100

    Napravljena glavna stranica sa dizajnom

---
+++ pub-kviz-front/src/Components/Event.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { FaTrashAlt } from 'react-icons/fa';
+import { useState } from 'react';
+
+function Event({data, inPrijave}) {  
+    const [teamName, setTeamName] = useState('');
+    const [teamMembers, setTeamMembers] = useState('');  
+
+    return (
+      <div className={inPrijave === 0 ? 'event' : 'eventb'}>
+          {inPrijave === 0 ? (
+          <>
+          
+          <div className='dataContainer'>
+            <div className='imageContainer'>
+                <img src={data.image.jpg} alt='Slika'/>
+            </div>
+
+              <div className='dataSubcontainer'>
+                  <h1>{data.title}</h1>
+                  <p className='tagline'>{data.tagline}</p>
+                  <p><span style={{fontWeight: 'bold'}}>Datum održavanja</span>: {data.date}</p>
+                  <p><span style={{fontWeight: 'bold'}}>Vreme održavanja</span>: {data.time}</p>
+                  <p><span style={{fontWeight: 'bold'}}>Mesto održavanja</span>: {data.location}</p>
+              </div>
+           </div>
+           <p>{data.description}</p>
+  
+          <div className='btns'>
+              
+  
+              <div className='btnsSub'>
+                <div style={{paddingRight:20}}>
+                <input style={{ height: 3+"em"}}
+                    type='text'
+                    placeholder='Naziv tima'
+                    value={teamName}
+                    onChange={(e) => setTeamName(e.target.value)}
+                />
+                </div>
+                <div style={{paddingRight:20}}>
+                <input style={{ height: 3+"em"}}
+                    type='number'
+                    placeholder='Broj članova'
+                    value={teamMembers}
+                    onChange={(e) => setTeamMembers(e.target.value)}
+                />
+                </div>
+                <button className={'btn1'}>
+                    <p>Prijavi tim</p>
+                </button>
+          </div>
+          </div>
+  
+          </>
+          ) : (
+          <>
+          <div className='dataContainer'>
+                <div className='imageContainer'>
+                    <img src={data.image.jpg} alt='Slika'/>
+                </div>
+  
+              <div className='dataSubcontainerb'>
+                  <h1 style={{marginLeft: 0.72 + "em"}}>{data.title}</h1>
+                  <p className='datab'>{data.location}</p>
+                  <p className='datab'>{data.date} • {data.time}</p>
+                  
+                      <p className='team' style={{marginLeft: 1.1 + "em"}}>Tim: {teamName}</p>
+                      
+              
+              </div>
+           </div>
+
+          <div className='btns'>
+              <div className='btnsSub'>
+              <button className='trash' >
+                  <FaTrashAlt className='btnb'/>
+              </button>
+          </div>    
+          </div>
+  
+          </>
+          )}
+      </div>
+    );
+  };
+  
+  export default Event;
---
+++ pub-kviz-front/src/Components/Events.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Event from './Event';
+
+function Events({events}) {
+    const currentEvent = events.slice(0, 5);
+  
+    return (
+      <div className='frame'>
+        <h1 className='subtitle'>DOSTUPNI PUB KVIZOVI</h1>
+        <div className='events'>
+          {currentEvent.map((event) => {
+            return <Event data = {event} key = {event.id} inPrijave = {0} />
+          })}
+        </div>
+        
+      </div>
+    );
+  };
+
+  export default Events;
